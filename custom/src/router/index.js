60ed9e85d5b81c425f075c029628f24312cd3ec6
miss-bu <3239424454@qq.com>
2019-11-28 09:12:23 +0800

    登陆页，保险预约首页

--- custom/src/router/index.js
+++ custom/src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import InsuerHome from '../views/InsureHome.vue'
+import InsuerInput from '../views/InsuerInput.vue'
+
 
 
 Vue.use(VueRouter)
@@ -25,7 +28,19 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/insureHome',
+    name: 'insureHome',
+    component: InsuerHome
+
+  },
+  {
+    path: '/InsuerInput',
+    name: 'InsuerInput',
+    component: InsuerInput
+
+  },
 ]
 
 const router = new VueRouter({
---
+++ custom/src/views/InsuerInput.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="app">
+        <div class="app-nav">
+           <li class="lis-l">  
+          <!--  <router-link to="/home/list">通讯录</router-link> -->
+         </li>
+         <span class=".span">保险信息</span>
+            
+             <li class="lis-r">  
+          <!--  <router-link to="/home/list">通讯录</router-link> -->
+         </li>
+        </div>
+        <div class="app-main">
+        <ul>
+        <li>
+            <span>关联车主卡：</span>
+            <span></span>
+        </li>
+         <li>
+            <span>车牌号码：</span>
+            <span></span>
+         </li>
+         <li>
+           <span>车架号：</span>
+            <span></span>
+         </li>
+        <li>
+        <span>发动机号：</span>
+            <span></span>
+
+        </li>
+        <li>
+            <span>除登日期：</span>
+            <span></span>
+        </li>
+        <li>
+            <span>品牌型号：</span>
+            <span></span>
+        </li>
+        <li>
+            <span>车主手机号：</span>
+            <span></span>
+        </li>
+        <li>
+            <span>身份证号：</span>
+            <span></span>
+        </li>
+        </ul>
+
+
+
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'insuerInput',
+    methords:{}
+    
+}
+</script>
+<style scoped>
+.app-nav{
+    width:100%;
+    height:60px;
+    background-color:black;
+   
+    }
+    .span{
+  color:white;
+  font-size: 20px;
+    line-height: 40px;
+    }
+.app-main{
+    width:100%;
+    height:1150px;
+}
+.lis-l{
+    width:100px;
+    height:100px;
+    border:1px solid white;
+    float:left;
+     list-style: none;
+}
+.lis-r{
+    width:100px;
+    height:100px;
+    border:1px solid white;
+    float:right;
+    list-style: none;
+
+}
+ul{
+    width:100%;
+    height:700px;
+    border:1px solid red;
+}
+li{
+    list-style: none;
+    height: 80px;
+    width:600px;
+    margin-left:30px;
+    border-bottom:1px solid rgb(129, 125, 125);
+}
+span{
+    font-size: 14px;
+    float:left;
+    line-height: 24px;
+
+}
+</style>
